4a451dffb2a0f5df7d33c5b556bcc87ec42321b3
Sukhchandan <sukhchandan.95@gmail.com>
2020-01-25 19:02:57 -0400

    commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count: 0,
+        tags: []
+    };
+    renderTags(){
+        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+        
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>;
+    }
+
+    render() { 
+        return(
+        <React.Fragment>
+            { this.state.tags.length === 0 && 'Please create a new tag'}
+           {this.renderTags()}
+        </React.Fragment>
+        );
+    }
+}
+ 
+export default Counter;
